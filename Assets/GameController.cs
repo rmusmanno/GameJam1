61c689a0f52f7fb556662e8acd846625e89625a1
Rafael Musmanno <rafa.musmanno@gmail.com>
2017-12-16 19:01:24 -0200

    End game controller, GameOver screen

---
+++ Assets/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	private void CleanupGame() {
+		// Do some cleanup
+	}
+
+	public void EndGame() {
+		CleanupGame ();
+		SceneManager.LoadScene ("GameOver");
+	}
+
+}
