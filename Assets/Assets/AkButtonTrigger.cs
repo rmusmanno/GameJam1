02c8a3f7cfa2a5dd25c701cb91b41b41734e8a06
Lucas <lucasferrazpg@gmail.com>
2017-12-17 12:51:43 -0200

    Play and Quit Button sounds

---
+++ Assets/Assets/AkButtonTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AkButtonTrigger:AkTriggerBase { 
+
+	void Start()
+	{
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(onClick);
+	}
+
+	void onClick() { 
+		if(triggerDelegate != null) { 
+			triggerDelegate(null); 
+		} 
+	} 
+}
